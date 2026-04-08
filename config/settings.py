06e0bdd150d4a92a164beb64db8268521ee42638
Abduvosit Qodirov <criro9891@gmail.com>
2026-04-08 09:39:58 +0500

    ready for deploy

--- config/settings.py
+++ config/settings.py
@@ -8,12 +8,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = Env()
 env.read_env()
 
+
 SECRET_KEY = env.str("SECRET_KEY")
+DEBUG = env.bool("DEBUG", default=False)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 
 # Application definition
