8c58e73131160438482f2905d2657a12626bd6f9
Abduvosit Qodirov <criro9891@gmail.com>
2026-04-08 15:10:51 +0500

    update scan count

--- app/views.py
+++ app/views.py
@@ -7,8 +7,15 @@ class SiteViewSet(ModelViewSet):
   queryset = Site.objects.all()
   serializer_class = SiteSerializer
 
+
 def redirect_to_site(request, pk):
-  site = get_object_or_404(Site, pk=pk)
-  site.scan_count += 1
-  site.save()
-  return redirect(site.url_site)
+    site = get_object_or_404(Site, pk=pk)
+
+    session_key = f"scanned_{pk}"
+
+    if not request.session.get(session_key):
+        site.scan_count += 1
+        site.save()
+        request.session[session_key] = True
+
+    return redirect(site.url_site)
